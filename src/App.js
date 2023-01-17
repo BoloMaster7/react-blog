54c4e069bf596947a22efb83cca59cb74ce0ed16
Pawel Godula <bolomaster7@gmail.com>
2023-01-17 19:05:42 +0100

    new components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/pages/Home/Home'
+import About from './components/pages/About/About';
+import NotFound from './components/pages/NotFound/NotFound';
+import PostEdit from './components/pages/PostEdit/PostEdit';
+import PostAdd from './components/pages/PostAdd/PostAdd';
+import { Routes, Route} from 'react-router-dom';
+import SinglePost from './components/pages/SinglePost/SinglePost';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/singlepost" element={<SinglePost/>} />
+    <Route path="/postadd" element={<PostAdd/>} />
+    <Route path="/postedit" element={<PostEdit/>} />
+    <Route path="*" element={<NotFound/>} />
+ </Routes>
+  </main>
   );
 }
-
 export default App;
---
+++ src/components/pages/About/About.js
@@ -0,0 +1,12 @@
+//import styles from './About.module.scss'
+import React from 'react';
+
+const About = ()  => {
+  return (
+    <div>
+
+        <p>Lorem ipsum</p>
+     </div>
+  );
+};
+export default About;
