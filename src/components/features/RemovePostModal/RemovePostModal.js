4145fc79e2b635500351560a9fd55d3519f21ebb
Pawel Godula <bolomaster7@gmail.com>
2023-01-22 16:42:22 +0100

    modal finished

--- src/components/features/RemovePostModal/RemovePostModal.js
+++ src/components/features/RemovePostModal/RemovePostModal.js
@@ -2,28 +2,25 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function RemovePostModal() {
-  const [show, setShow] = useState(false);
+function RemovePostModal(props) {
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+    
+      <Modal show={props.showModal} 
+      onHide={props.handleClose}
+      backdrop="static"
+      keyboard={false}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Are you sure?</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={props.handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={props.handleClose}>
             Save Changes
           </Button>
         </Modal.Footer>
--- src/components/pages/SinglePost/SinglePost.js
+++ src/components/pages/SinglePost/SinglePost.js
@@ -5,9 +5,13 @@ import { getPostById } from '../../../redux/postsRedux';
 import { Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { removePost } from '../../../redux/postsRedux';
+import { Link } from 'react-router-dom';
+//import RemovePostModal from '../../features/RemovePostModal/RemovePostModal';
+import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { Navigate } from 'react-router-dom';
 
-
-const SinglePost = ()  => {
+const SinglePost = (props)  => {
   const { id } = useParams();
   const postsData = useSelector(state => getPostById(state, id))
 
@@ -15,11 +19,16 @@ const SinglePost = ()  => {
 
   const deletePost = (e) => {
     e.preventDefault();
-    dispatch(removePost(id));
+   dispatch(removePost(id));
   };
 
+  const [show, setShow] = useState(false);
 
-
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true)
+  
+  if(!postsData) return <Navigate to="/" />
+  else 
   return (
     <div className='col-md-8'>
      <header className='d-flex justify-content-between mt-2 mb-2 p-2' >    
@@ -28,12 +37,32 @@ const SinglePost = ()  => {
           </h2>
           <div>
           <Button variant="btn btn-outline-primary m-1" a href='/post/edit/id' >Edit</Button>
-          <Button variant="btn btn-outline-danger m-1">Delete</Button>
+          <Link key={props.id} to={'/post/edit/' + props.id}>
+          <Button variant="primary">Read more</Button>
+        </Link>
+          <Button variant="btn btn-outline-danger m-1" onClick={handleShow}
+        >Delete</Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>This will delete your post!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel  
+          </Button>
+          <Button variant="danger" onClick={deletePost}>Remove</Button>
+        </Modal.Footer>
+      </Modal>
           </div >
           </header>
           <p>Author: {postsData.author}</p>
           <p>Published: {postsData.publishedDate}</p>
           <p >{postsData.shortDescription}</p> 
+
+
+
     </div>
   );
 };
