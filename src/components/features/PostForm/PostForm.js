ed2e12ef58a9359b92722327e26e7473293886e3
Pawel Godula <bolomaster7@gmail.com>
2023-01-27 15:15:46 +0100

    validate date on form

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -15,11 +15,16 @@ import { useForm } from "react-hook-form";
     const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
     const [content, setContent] = useState(props.content || '');
     const { register, handleSubmit: validate, formState: { errors } } = useForm();
+    const [contentError, setContentError] = useState(false);
+    const [dateError, setDateError] = useState(false)
 
-  const handleSubmit = e=> {
-  
-    action({ title, author, publishedDate, shortDescription, content });
-  };
+    const handleSubmit = () => {
+      setContentError(!content)
+      setDateError(!publishedDate)
+      if(content && publishedDate) {
+        action({ title, author, publishedDate, shortDescription, content });
+      }
+    };
 
   return (
     <>
@@ -27,18 +32,24 @@ import { useForm } from "react-hook-form";
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Title</Form.Label>
           <Form.Control
-            {...register("title", { required: true })}
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            type="text" placeholder="Enter title"
-          />
-          {errors.title && <small className="d-block form-text text-danger mt-2">This field is required</small>}
+          {...register("title", { required: true, minLength: 3 })}
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text" placeholder="Enter title"
+        />
+        {errors.title && <small className="d-block form-text text-danger mt-2">
+          Title is too short (min is 3)</small>}
 </Form.Group>
 
       <Form.Group className="mb-3" >
         <Form.Label>Author</Form.Label>
-        <Form.Control type="text" placeholder="Author" value={author}
+        <Form.Control 
+        {...register("author", { required: true, minLength: 3 })}
+        value={author}
+        type="text" placeholder="Author" 
         onChange={(e) => setAuthor(e.target.value)}/>
+         {errors.author && <small className="d-block form-text text-danger mt-2">
+          Author is too short (min is 3)</small>}
       </Form.Group>
      
       <Form.Group className="mb-3" >
@@ -47,16 +58,24 @@ import { useForm } from "react-hook-form";
       selected={publishedDate}
       onChange={date => setPublishedDate(date)}
       />
+      {dateError && <small className="d-block form-text text-danger mt-2">
+        Date can't be empty</small>}
       </Form.Group>
 
       <Form.Group className="mb-3" >
         <Form.Label>Short Description</Form.Label>
-        <Form.Control type="text" placeholder="Leave a comment here" value ={shortDescription}
+        <Form.Control 
+         {...register("shortDescription", { required: true, minLength: 20 })}
+        type="text" placeholder="Leave a comment here" value ={shortDescription}
         onChange={(e) => setShortDescription(e.target.value)}/>
+          {errors.shortDescription && <small className="d-block form-text text-danger mt-2">
+          Description is too short (min is 20)</small>}
       </Form.Group>
 
       
       <ReactQuill value={content} onChange={setContent} />
+      {contentError && <small className="d-block form-text text-danger mt-2">
+        Content can't be empty</small>}
 
       <Button variant="primary" onClick={handleSubmit}>
         {actionText}
